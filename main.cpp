43d8fb4bc418f96e121a3d755d534a9e888acb0f
krvnbards <kervin.bardilas@gmail.com>
2025-03-20 20:18:01 +0800

    Bago

--- main.cpp
+++ main.cpp
@@ -1,25 +1,97 @@
 #include <iostream>
-#include <limits>
+#include <iomanip>
+#include <sstream>
 
 using namespace std;
 
+
+
+//[--------------------------VARIABLES---------------------------]
+
+// Gamit ta ug char diri para inig cout kay char mo gawas
+// Plano pud nako nga naay option nga unsay character or symbol ang ganahan for player
+char board[3][3] = {
+	{'1', '2', '3'},
+	{'4', '5', '6'},
+	{'7', '8', '9'}
+};
+
+enum stat_list {
+	
+	MAINMENU = 1,
+	HOW_TO_PLAY,
+	DEVELOPERS
+};
+
+enum stat_list STATUS = MAINMENU;
+//[--------------------------------------------------------------]
+
+//[----------------------------FUNCTION DECLARATIONS----------------------------]
+void DisplayMenu();
+//[-----------------------------------------------------------------------------]
+
+
 int main() {
-	
-	int action;
-	
-	cout << "Enter number from 1-4: ";
-	cin >> action;
-	
-	if(cin.fail() || action < 0 || action > 4) {
-		cout << "You have entered an invalid input (character)!";
-		return 1;
+	system("color 0a");
+	while(1) {
+		switch(STATUS) {
+			case MAINMENU: {
+				DisplayMenu();
+				break;
+			}
+		}
 	}
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	string name;
-	cout << "Enter name: ";
-	cin >> name;
-	
-	cout << "Action: " << action << endl << "Name: " << name;
-	
 	return 1;
 }
+
+//[----------------------------FUNCTION DEFINITIONS----------------------------]
+void DisplayMenu() {
+	system("cls");
+	
+	string TicTacToeArt = R"(                                                                                                                                               
+
+  _____ _     _____         _____          
+ |_   _(_) __|_   _|_ _  __|_   _|__   ___ 
+   | | | |/ __|| |/ _` |/ __|| |/ _ \ / _ \
+   | | | | (__ | | (_| | (__ | | (_) |  __/
+   |_| |_|\___||_|\__,_|\___||_|\___/ \___|
+                                           
+                                                                                                                                                                                                    
+	)";
+	
+	istringstream stream(TicTacToeArt);
+    string line;
+    while (getline(stream, line)) {
+        std::cout << std::setw(50) << line << std::endl; // Adjust 50 for alignment
+    }
+	
+	cout << "================================================================================================\n";
+	
+	int status;
+	
+	cout << R"(	
+ ___        ______ __                   _______                       
+|_   |      |   __ \  |.---.-.--.--.    |     __|.---.-.--------.-----.
+ _|  |_     |    __/  ||  _  |  |  |    |    |  ||  _  |        |  -__|
+|______|    |___|  |__||___._|___  |    |_______||___._|__|__|__|_____|
+                             |_____|                                   
+
+	)";
+	
+	cout << R"(	
+
+ ______      _______                       _______               ______ __              
+|__    |    |   |   |.-----.--.--.--._____|_     _|.-----.______|   __ \  |.---.-.--.--.
+|    __|    |       ||  _  |  |  |  |______||   |  |  _  |______|    __/  ||  _  |  |  |
+|______|    |___|___||_____|________|       |___|  |_____|      |___|  |__||___._|___  |
+                                                                                 |_____|
+
+	)";
+	 
+	//cout << "[1] Play Game\n[2] How to Play\n[3] Developers\n[4] Settings\n[5] Exit\n\n" << endl;
+	cout << "Enter action: ";
+	while(cin >> status) {
+		
+	}
+}
+//[----------------------------------------------------------------------------]
