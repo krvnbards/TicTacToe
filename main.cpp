70338ff51a864026bcb15fbf6484f16e8490a6d6
kkb13 <mooremarshall13@gmail.com>
2025-03-20 18:44:26 +0800

    Updated all

--- main.cpp
+++ main.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 int main() {
 	
-	// Tic Tac Toe Game here
+	int action;
+	
+	cout << "Enter number from 1-4: ";
+	cin >> action;
+	
+	if(cin.fail() || action < 0 || action > 4) {
+		cout << "You have entered an invalid input (character)!";
+		return 1;
+	}
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	string name;
+	cout << "Enter name: ";
+	cin >> name;
+	
+	cout << "Action: " << action << endl << "Name: " << name;
+	
 	return 1;
 }
